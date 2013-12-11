7dff2d06c67bb10e0eaa3e9f629a9b07f8c7d7d2
Chris Go <chris@velocimedia.com>
2013-12-11 09:38:11 -0800

    Add more characters to regex

--- init.php
+++ init.php
@@ -6,7 +6,7 @@ require_once Kohana::find_file('vendor/SabreDAV-1.8.7/vendor','autoload','php');
 // route for demo controller
 Route::set('webdav', 'webdav(/<id>)',
 	array(
-		'id' => '[a-zA-Z0-9_/]+',
+		'id' => '[/.a-zA-Z0-9-_ ]+',
 	))
 	->defaults(array(
 		'controller' => 'Webdav',
